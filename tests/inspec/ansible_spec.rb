588ca5859abce19e0c54c9863377c68a57d7506c
diodonfrost <diodon.frost@diodonfrost.me>
2019-04-17 23:40:11 +0200

    fixing ansible version with python pip

--- tests/inspec/ansible_spec.rb
+++ tests/inspec/ansible_spec.rb
@@ -6,12 +6,16 @@ control 'ansible-01' do
   impact 1.0
   title 'Ansible install'
   desc 'Ansible should be installed'
-  describe.one do
-    describe package(ansible_package) do
-      it { should be_installed }
-    end
-    describe pip(ansible_package) do
-      it { should be_installed }
-    end
+  describe pip(ansible_package) do
+    it { should be_installed }
+  end
+end
+
+control 'ansible-02' do
+  impact 1.0
+  title 'Ansible command'
+  desc 'Ansible command should be present'
+  describe command('ansible --version') do
+    its('exit_status') { should eq 0 }
   end
 end
