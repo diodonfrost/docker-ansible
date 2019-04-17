0df32da38e5907c18ec3f56860093d32c2a47054
diodonfrost <diodon.frost@diodonfrost.me>
2019-04-17 23:48:39 +0200

    change inspec tests

--- tests/inspec/ansible_spec.rb
+++ tests/inspec/ansible_spec.rb
@@ -4,15 +4,6 @@ ansible_package = "ansible"
 
 control 'ansible-01' do
   impact 1.0
-  title 'Ansible install'
-  desc 'Ansible should be installed'
-  describe pip(ansible_package) do
-    it { should be_installed }
-  end
-end
-
-control 'ansible-02' do
-  impact 1.0
   title 'Ansible command'
   desc 'Ansible command should be present'
   describe command('ansible --version') do
