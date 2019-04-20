f8041c8f9729d5815404818a12b8b1e29d5d9225
diodonfrost <diodon.frost@diodonfrost.me>
2019-04-20 20:05:50 +0200

    Revert to default Ansible os package

--- tests/inspec/ansible_spec.rb
+++ tests/inspec/ansible_spec.rb
@@ -4,9 +4,14 @@ ansible_package = "ansible"
 
 control 'ansible-01' do
   impact 1.0
-  title 'Ansible command'
-  desc 'Ansible command should be present'
-  describe command('ansible --version') do
-    its('exit_status') { should eq 0 }
+  title 'Ansible install'
+  desc 'Ansible should be installed'
+  describe.one do
+    describe package(ansible_package) do
+      it { should be_installed }
+    end
+    describe pip(ansible_package) do
+      it { should be_installed }
+    end
   end
 end
