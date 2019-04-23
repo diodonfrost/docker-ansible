8c188f44277b8decdfffedabd3e81faa056ec788
diodonfrost <diodon.frost@diodonfrost.me>
2019-04-23 17:30:32 +0200

    improve inspec tests

--- tests/inspec/ansible_spec.rb
+++ tests/inspec/ansible_spec.rb
@@ -1,17 +1,29 @@
 # Test for Ansible docker container
 
 ansible_package = "ansible"
+package_resource_dont_supported_os = ['gentoo']
 
 control 'ansible-01' do
   impact 1.0
   title 'Ansible install'
   desc 'Ansible should be installed'
-  describe.one do
-    describe package(ansible_package) do
-      it { should be_installed }
+  unless package_resource_dont_supported_os.include? os.name
+    describe.one do
+      describe package(ansible_package) do
+        it { should be_installed }
+      end
+      describe pip('ansible') do
+        it { should be_installed }
+      end
     end
-    describe pip(ansible_package) do
-      it { should be_installed }
-    end
+  end
+end
+
+control 'ansible-02' do
+  impact 1.0
+  title 'Ansible command'
+  desc 'Ansible command should be functionnal'
+  describe command('ansible --version') do
+    its('exit_status') { should eq 0 }
   end
 end
